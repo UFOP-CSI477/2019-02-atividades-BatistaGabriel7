3ac89d658391395c51fff1ea9bd4c3af1bdff88a
Gabriel Batista Araujo Almeida <gbaa98@gmail.com>
2019-10-28 15:26:34 -0300

    1.3 concluído

--- CSI477-2019-02-atividade-pratica-001/1.3/calculo.js
+++ CSI477-2019-02-atividade-pratica-001/1.3/calculo.js
@@ -29,33 +29,31 @@ function calcular() {
 
     if (validar(n1, "alerta1", "label1") && validar(n2, "alerta2", "label2")) {
         let res = Math.log10(parseFloat(n1.value)) + 3 * (Math.log10(8 * parseFloat(n2.value))) - 2.92;
+        console.log(res);
 
         if (res < 3.5) {
             document.dados.classificacao.value = "Imperceptível.";
         } else if (res >= 3.5 && res <= 5.4) {
             document.dados.classificacao.value = "Pequeno.";
-        } else if (res >= 5.5 && res <= 6) {
+        } else if (res > 5.4 && res <= 6) {
             document.dados.classificacao.value = "Moderado.";
-        } else if (res >= 6.1 && res <= 6.9) {
+        } else if (res > 6.0 && res <= 6.9) {
             document.dados.classificacao.value = "Forte.";
-        } else if (res >= 7.0 && res <= 7.9) {
+        } else if (res > 6.9 && res <= 7.9) {
             document.dados.classificacao.value = "Grande.";
         } else {
             document.dados.classificacao.value = "Enorme.";
         }
 
-        let min = (n2.value * n2.value) * 18.5;
-        let max = (n2.value * n2.value) * 24.9;
-
         if (res < 3.5) {
             document.dados.efeitos.value = "Geralmente não sentido, mas gravado.";;
         } else if (res >= 3.5 && res <= 5.4) {
             document.dados.efeitos.value = "Às vezes sentido, mas raramente causa danos";
-        } else if (res >= 5.5 && res <= 6) {
+        } else if (res > 5.4 && res <= 6) {
             document.dados.efeitos.value = "Pequenos danos a prédios bem construídos, mas pode danificar seriamente casas mal construídas.";
-        } else if (res >= 6.1 && res <= 6.9) {
+        } else if (res > 6.0 && res <= 6.9) {
             document.dados.efeitos.value = "Pode ser destrutivo em áreas em torno de até 100km do epicentro.";
-        } else if (res >= 7.0 && res <= 7.9) {
+        } else if (res > 6.9 && res <= 7.9) {
             document.dados.efeitos.value = "Grande terremoto. Pode causar sérios danos numa grande faixa.";
         } else {
             document.dados.efeitos.value = "Enorme terremoto. Pode causar graves danos em muitas áreas mesmo que estejam a centenas de quilômetros.";
